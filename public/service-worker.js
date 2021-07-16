9757e193261a6598f67804658e1f4be643f47f9a
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-07-16 18:22:37 -0500

    edited service worker filer

--- public/service-worker.js
+++ public/service-worker.js
@@ -60,6 +60,15 @@ const FILES_TO_CACHE = [
                             return cache.match(evt.request);
                         });
                 })
-            )
-        };
+            );
+            return;
+        }
+
+        evt.respondWith(
+            caches.open(CACHE_NAME).then( cache => {
+                return cache.match(evt.request).then(response => {
+                    return response || fetch(evt.request)
+                });
+            })
+        );
     });
