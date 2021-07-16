01dccfbea77f2084b05b3245c20fa24ded744bf1
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-07-16 18:05:01 -0500

    debugging some changes

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
 const express = require("express");
-const logger = require("logger");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/budget",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    },
-);
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-//routes
-
+// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
