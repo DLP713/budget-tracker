2239e259a684ac95b8fea8e08b750c2c534a2918
Cesar De La Portilla <cesar.dlp135@gmail.com>
2021-07-16 16:57:29 -0500

    edited minor error

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("logger");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const e = require("express");
 
 const PORT = process.env.PORT || 3000;
 
